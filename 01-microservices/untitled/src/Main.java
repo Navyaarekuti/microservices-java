b090c8c7b7e35f79763cabbdb5324a5089908945
Navyaarekuti <arekutinavya1@gmail.com>
2024-06-13 14:23:54 -0500

    files

---
+++ 01-microservices/untitled/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String args[])
+    {
+        String sentence = "Tim to write great code";
+        EvenString e = new EvenString();
+        System.out.println(e.longestEvenString(sentence));
+
+    }
+
+}
